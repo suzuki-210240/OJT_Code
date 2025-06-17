038ccf06097848cb18d7b1b4cba05222321989a8
test <tejc210240@gmail.com>
2025-06-17 21:19:04 +0900

    更新2

--- Ex/Ex01.java
+++
@@ -1,7 +0,0 @@
-package OJT_Code.Ex;
-
-public class Ex01 {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
---
+++ Example/src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    public String name;
+    public int age;
+
+    public String show(){
+        return "A";
+    }
+
+    public String toString(){
+        return name + " " + age;
+    }
+}
